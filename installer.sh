7a803cf8ec87c05c7abddcbc23c68bb0dfe95c40
JaDykman <jacobdykman@gmail.com>
2025-05-01 20:19:38 -0500

    Revert "Fixed run issue"
    This reverts commit e01ad2a745af07da13449e44ac25f09c53887cdd.

--- installer.sh
+++ installer.sh
@@ -91,9 +91,6 @@ EOL
 # Make the update checker executable
 chmod +x update_checker.py
 
-# Make the run.sh script executable
-chmod +x run.sh
-
 # Setup cron job automatically
 echo "Setting up automatic update checks via cron..."
 CURRENT_DIR=$(pwd)
